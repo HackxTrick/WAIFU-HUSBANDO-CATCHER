877e1701f124b8aa3358b870af524554d81dac9b
HackxTrick <153048345+HackxTrick@users.noreply.github.com>
2024-03-12 22:57:27 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
+    OWNER_ID = "1910728581"
+    sudo_users = "1910728581"
+    GROUP_ID = -1002106866999
     TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    mongo_url = "mongodb+srv://Hackx:Hackx@hackx.6flteju.mongodb.net/?retryWrites=true&w=majority&appName=Hackx"
+    PHOTO_URL = ["https://graph.org/file/d4c3e0cfdea337d761f6f.jpg", "https://graph.org/file/d4c3e0cfdea337d761f6f.jpg"]
+    SUPPORT_CHAT = "GAURAV_BOTS"
+    UPDATE_CHAT = "GAURAV_BOTS"
+    BOT_USERNAME = "Infinite_waifus_bot"
+    CHARA_CHANNEL_ID = "-1002007169772"
+    api_id = 28837889
+    api_hash = "9d5e9c5b8abcf8b7b930abd259de254e"
 
     
 class Production(Config):
